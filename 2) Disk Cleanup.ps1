d9a993b1538ca9473fdb4a2c7f0e9da280c9029d
TwittleFingers <100237000+TwittleFingers@users.noreply.github.com>
2024-07-02 11:46:10 -0700

    Add files via upload

---
+++ 2) Disk Cleanup.ps1
@@ -0,0 +1,19 @@
+﻿
+# Set the disk cleanup options
+$cleanupOptions = @(
+    "Temporary Internet Files"    # Temp files and logs
+    "System Error Memory Dump"
+    "System Error Memory Dump Files"
+    "Hibernation File"
+    "Windows Upgrade Log Files"
+    "Delivery Opt-in Assistant Synchronization Files"
+    "Prefetch"
+    "Recycle Bin"
+)
+
+# Run disk cleanup with the specified options
+CleanMgr /sagerun:100 /a $cleanupOptions
+
+# Set the verbosity level to 2 for more detailed output
+$VerbosePreference = 2
+
