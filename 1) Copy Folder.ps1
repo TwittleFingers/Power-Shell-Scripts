11947616da8ad9f40f6555a8877b7abf09957522
TwittleFingers <100237000+TwittleFingers@users.noreply.github.com>
2024-07-02 11:45:44 -0700

    Add files via upload

---
+++ 1) Copy Folder.ps1
@@ -0,0 +1,22 @@
+﻿
+# 07.02.24
+# Copy Folder one location to another
+# Used to template folders for ease of use
+# Define the source and destination folders
+
+
+#Source Folder
+$sourceFolder = "C:\Users\bjulao\Desktop\00 - Project Template Folder"
+
+#Destination Folder
+$destinationFolder = "C:\Users\bjulao\Documents"
+
+# Check if the destination folder exists, if not create it
+if (!(Test-Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destinationFolder
+}
+
+# Copy the folder and its contents
+Copy-Item -Path $sourceFolder -Destination $destinationFolder -Recurse -Force
+
+
